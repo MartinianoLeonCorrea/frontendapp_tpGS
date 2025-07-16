9a7e0e911a0b2a78c61b542215bd4132af6ac570
Lara <laravarrenti@gmail.com>
2025-07-16 11:39:26 -0300

    Registrando botones con middleware

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 console.log('Este archivo va a ser interpretado por el navegador');
 
 //Obtener elementos del HTML y guardarlos en constantes
-const button = document.querySelector('button');
-console.log({ button });
+const getBtn = document.querySelector('#get-task');
+const createBtn = document.querySelector('#create-task');
+const input = document.querySelector('#task-name');
 
 //Nutrir de funcionalidad los botones
-button.addEventListener('click', () => {
-  console.log('CLick');
-  fetch('http://localhost:4000/users');
+getBtn.addEventListener('click', function () {
+  console.log('Get tareas');
+  fetch('http://localhost:4000/api/tasks');
+});
+
+createBtn.addEventListener('click', function () {
+  console.log('Crear tarea');
+  console.log({ input });
+  fetch('http://localhost:4000/api/tasks', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ text: input.value }),
+  });
 });
